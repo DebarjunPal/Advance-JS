04bc2893bd9ef2f3c8c6495e476e7bf5b2a6ab63
Debarjun Pal <116552204+DebarjunPal@users.noreply.github.com>
2024-04-07 07:45:06 +0000

    added 01_arrays.js file under 02_basics folder

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+//Arrays
+
+//Slice and Splice:
+
+//Original Array:
+const myArr = [0, 1, 2, 3, 4, 5]
+console.log(myArr);
+//Slice:
+const myArr1 = myArr.slice(1,3)
+console.log(myArr1);
+console.log("A :",myArr);
+//Splice:
+const myArr2 = myArr.splice(1,3)
+console.log(myArr2);
+console.log("B :",myArr);
