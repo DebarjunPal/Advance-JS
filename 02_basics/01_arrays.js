891923d8f592150b7bbf35717f45eea975c95ccf
Debarjun Pal <116552204+DebarjunPal@users.noreply.github.com>
2024-04-07 07:51:34 +0000

    modified:   02_basics/01_arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,12 +4,11 @@
 
 //Original Array:
 const myArr = [0, 1, 2, 3, 4, 5]
-console.log(myArr);
 //Slice:
 const myArr1 = myArr.slice(1,3)
 console.log(myArr1);
-console.log("A :",myArr);
+console.log("Original Array :",myArr);
 //Splice:
 const myArr2 = myArr.splice(1,3)
 console.log(myArr2);
-console.log("B :",myArr);
+console.log("Original Array :",myArr);
